c3abf3c9e08b76258dc4538af501cac4f1015110
jaggehns <jaggehns@gmail.com>
2021-10-09 20:42:43 +0800

    Implemented Topbar component

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/9190637/pexels-photo-9190637.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
