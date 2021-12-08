4b679f522ab997e37e1f6551b6f80b6b78921b20
jaggehns <jaggehns@gmail.com>
2021-12-09 02:07:12 +0800

    Implemented featuredInfo component

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
+}
