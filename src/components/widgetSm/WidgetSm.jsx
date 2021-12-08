e110c6c56f5954535061bc4dd71fdcc12cb610fa
jaggehns <jaggehns@gmail.com>
2021-12-09 04:22:21 +0800

    Implemented widgets

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,87 @@
+import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/9190637/pexels-photo-9190637.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/9190637/pexels-photo-9190637.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/9190637/pexels-photo-9190637.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/9190637/pexels-photo-9190637.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/9190637/pexels-photo-9190637.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 export default function Home() {
   return (
@@ -13,6 +15,10 @@ export default function Home() {
         grid
         dataKey="Active User"
       />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 }
